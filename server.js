6daff55ef162bf9a540607ff777b749a89a94c06
Hông Duc TA <hong-duc.ta@protonmail.com>
2019-02-21 11:16:12 +0100

    fix server

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ server.listen(PORT, () => console.log('The bot server is running on port ' + POR
 */
 
 let u = new FBeamer(fb);
-server.get('/', (req,res) => {u.registerHook(req,res);res.send("hello !");});
+server.get('/', (req,res) => {u.registerHook(req,res);});
 server.listen(PORT,() => console.log('Ok'));
